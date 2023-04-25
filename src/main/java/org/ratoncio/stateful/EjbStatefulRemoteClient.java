0e334acb8f6c8562d3c0bfbb0e3ec07e26f11289
Javier Solabre <javier.solabre@thomsonreuters.com>
2023-04-25 15:02:06 +0200

    add jndi for stateful

--- src/main/java/org/ratoncio/stateful/EjbStatefulRemoteClient.java
+++ src/main/java/org/ratoncio/stateful/EjbStatefulRemoteClient.java
@@ -37,6 +37,8 @@ public class EjbStatefulRemoteClient extends HttpServlet{
         jndi = "org.ratoncio.stateful.EjbStatefulRemote";
         jndi = "ejb/ejb-app/ejb-app.war/EjbStatefulBean#org.ratoncio.stateful.EjbStatefulRemote";
         jndi = "java:global/ejb-app/EjbStatefulBean!org.ratoncio.stateful.EjbStatefulRemote";
+        jndi = "ejb/global/ejb-app/EjbStatefulBean!org.ratoncio.stateful.EjbStatefulRemote";
+
 
         System.out.println("[JNDI]: " + jndi);
         remoteObject = remoteContext.lookup(jndi);
